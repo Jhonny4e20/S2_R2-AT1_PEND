35e3812d9b258c8d5237d80e87f2d7be5f092887
JOÃO PEDRO BARBOSA KOVALSKI <joao.kovalski2@senaisp.edu.br>
2026-03-13 12:36:29 -0300

    ATV de Checkbox da Lista de Tarefa

--- script.js
+++ script.js
@@ -16,8 +16,28 @@ botao.addEventListener("click", function(){
     const novaTarefa = document.createElement("li");
     novaTarefa.className = "list-group-item d-flex justify-content-between align-items-center";
 
+    const container = document.createElement("div");
+    container.className = "d-flex align-items-center gap-2";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "form-check-input";
+
     const span = document.createElement("span");
     span.textContent = texto;
+
+    checkbox.addEventListener("change", function(){
+
+        if(checkbox.checked){
+            span.style.textDecoration = "line-through";
+            span.style.color = "gray";
+        }else{
+            span.style.textDecoration = "none";
+            span.style.color = "black";
+            
+        }
+
+    });
 
     const botaoRemover = document.createElement("button");
     botaoRemover.textContent = "Remover";
@@ -27,7 +47,10 @@ botao.addEventListener("click", function(){
         novaTarefa.remove();
     });
 
-    novaTarefa.appendChild(span);
+    container.appendChild(checkbox);
+    container.appendChild(span);
+
+    novaTarefa.appendChild(container);
     novaTarefa.appendChild(botaoRemover);
 
     lista.appendChild(novaTarefa);
