da105d52b597af020b9195a1742c4878d5080076
JOÃO PEDRO BARBOSA KOVALSKI <joao.kovalski2@senaisp.edu.br>
2026-03-13 10:06:41 -0300

    ATV de Criar Lista

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const texto = document.getElementById("texto");
-const botao = document.getElementById("botao");
+const input = document.getElementById("itemInput");
+const botao = document.getElementById("botaoAdicionar");
+const lista = document.getElementById("lista");
 
 botao.addEventListener("click", function(){
 
-    texto.textContent = "Texto alterado com a ✨Magia✨ da Programação🫴💫";
+    const textoItem = input.value;
+
+    const novoItem = document.createElement("li");
+
+    novoItem.textContent = textoItem;
+
+    lista.appendChild(novoItem);
+
+    input.value = "";
 
 });
