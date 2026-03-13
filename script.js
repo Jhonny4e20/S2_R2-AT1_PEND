dc9f9ac488625caa30df150236028b3f80e8d8d8
JOÃO PEDRO BARBOSA KOVALSKI <joao.kovalski2@senaisp.edu.br>
2026-03-13 10:01:49 -0300

    ATV mudar texto

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const texto = document.getElementById("texto");
-const botao = document.getElementById("botao");
 
-botao.addEventListener("click", function(){
+
+const texto = document.getElementById("texto");
+const botaoMudar = document.getElementById("botaoMudar");
+const botaoResetar = document.getElementById("botaoResetar");
+
+botaoMudar.addEventListener("click", function(){
 
     texto.textContent = "Texto alterado com a ✨Magia✨ da Programação🫴💫";
+
+});
+
+botaoResetar.addEventListener("click", function(){
+
+    texto.textContent = "Clique no botão para mudar este texto.";
 
 });
